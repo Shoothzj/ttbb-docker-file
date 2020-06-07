45b041e0823ed15f13b17d5b4e9cfe48005ff9e4
shoothzj <shoothzj@gmail.com>
2020-06-07 17:23:30 +0800

    add skywalking:

--- skywalking/stand-alone/source/scripts/start.sh
+++ skywalking/stand-alone/source/scripts/start.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-# SW_STORAGE=elasticsearch7
-# docker run -e SW_STORAGE_ES_CLUSTER_NODES='****' -p 8080:8080 -p 12800:12800 -d ttbb/skywalking:stand-alone
+# docker run -p 8080:8080 -p 12800:12800 -d ttbb/skywalking:stand-alone
 cd "$(dirname "$0")"
 
 cd ..
 
 echo `pwd`
-
-cp /opt/sh/config/application.yaml /opt/sh/skywalking/config/application.yml
 
 /opt/sh/skywalking/bin/startup.sh
 
