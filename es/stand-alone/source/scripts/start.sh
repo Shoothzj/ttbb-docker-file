fec5dec56c6dd65a8f7039113130f3465039db39
shoothzj <shooothzj@gmail.com>
2020-07-17 20:27:08 +0800

    add integrate

--- es/stand-alone/source/scripts/start.sh
+++ es/stand-alone/source/scripts/start.sh
@@ -5,8 +5,4 @@ cd ..
 
 echo `pwd`
 
-sudo bash /opt/sh/scripts/sudo.sh
-
-cp /opt/sh/conf/elasticsearch.yml /opt/sh/elasticsearch/config/elasticsearch.yml
-
 /opt/sh/elasticsearch/bin/elasticsearch
